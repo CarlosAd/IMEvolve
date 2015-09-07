e630f3349e270b75579b803c45cf4d00be312e85
CarlosAd <carlos.adpinto@outlook.com>
2015-09-06 22:51:30 -0300

    Criada a classe virtual que define a funcão objetivo.

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/main.o"
+  "CMakeFiles/main.dir/src/fitnessFunction.o"
+  "bin/main.pdb"
+  "bin/main"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ include/fitnessFunction.h
@@ -0,0 +1,9 @@
+#ifndef FITNESS_FUNCTION_H
+#define FITNESS_FUNCTION_H
+
+class FitnessFunction {
+public:
+  virtual double operator()() = 0;
+};
+
+#endif
