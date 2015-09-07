922e8e94cd71066504283ac2799bb7b7785c5879
CarlosAd <carlos.adpinto@outlook.com>
2015-09-06 23:20:13 -0300

    Deletados os arquivos em inglês.

--- include/fitnessFunction.h
+++
@@ -1,9 +0,0 @@
-#ifndef FITNESS_FUNCTION_H
-#define FITNESS_FUNCTION_H
-
-class FitnessFunction {
-public:
-  virtual double operator()() = 0;
-};
-
-#endif
--- src/fitnessFunction.cpp
+++
@@ -1,1 +0,0 @@
-#include "fitnessFunction.h"
