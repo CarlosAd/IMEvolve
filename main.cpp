3f1cdf2298a3023989550fce6326ec3052ba27cd
CarlosAd <carlos.adpinto@outlook.com>
2015-09-30 09:07:41 -0300

    Definidos o construtor e o método para gerar população da classe 'Geração'.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,4 @@
-#include "../include/individuo.h"
-#include <iostream>
+#include "geracao.h"
 
-int main () {
-  Individuo i (10);
-  for (int index = 0; index < i.tamanho(); index++) {
-    std::cout << static_cast<int>(i[index]) << std::endl;
-  }
-  std::cout << "Hello Darwin!" << std::endl;
-
-  return 0;
+int main() {
 }
--- src/geracao.cpp
+++ src/geracao.cpp
@@ -1,1 +1,23 @@
 #include "geracao.h"
+
+Geracao::Geracao(FuncaoObjetivo &ifuncaoObjetivo,
+                 SeletorNatural &iseletorNatural,
+                 FuncaoMutacao &ifuncaoMutacao,
+                 FuncaoReproducao &ifuncaoReproducao) :
+                 funcaoObjetivo (ifuncaoObjetivo),
+                 seletorNatural (iseletorNatural),
+                 funcaoMutacao (ifuncaoMutacao),
+                 funcaoReproducao (ifuncaoReproducao) {
+}
+
+void Geracao::gerarPopulacao(const unsigned int& itamanhoCromossomo,
+                             const unsigned int& itamanhoPopulacao,
+                             const unsigned int& ielitismo) {
+  tamanhoPopulacao = itamanhoPopulacao;
+  elitismo = ielitismo;
+  populacaoAuxiliar.resize(tamanhoPopulacao - elitismo, Individuo(itamanhoCromossomo));
+
+  for (int i = 0; i < itamanhoPopulacao; i++) {
+    populacao.push_back(Individuo(itamanhoCromossomo));
+  }
+}
